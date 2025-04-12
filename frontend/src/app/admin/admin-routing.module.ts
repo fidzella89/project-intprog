d25248135b1732706e6373a3d5ceb4abfd73b031
CryoYohan <cyrilypil@gmail.com>
2025-04-12 19:40:47 +0800

    [feat]: Admin Routing Module

---
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouteModule } from '@angular/router';
+
+import { SubNavComponent } from './subnav.component';
+import { LayoutComponent } from './layout.component';
+import { OverviewComponent } from './overview.component';
+
+const accountsModule = () => import('./accounts/accounts.module').then(x => x.AccountsModule);
+
+const routes: Routes = [
+    { path: '', component: SubNavComponent, outlet: 'subnav' },
+    {
+        path: '', component: LayoutComponent,
+        children: [
+            { path: '', component: OverviewComponent },
+            { path: 'accounts', loadChildren: accountsModule }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouteModule.forChild(routes)],
+    export: [RouteModule]
+})
+export class AdminRoutingModule { }
