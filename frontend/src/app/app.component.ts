5d27f7ec09d753e420fac5df9a81d2249ef63cd3
jimonte30 <jherickisamar@gmail.com>
2025-05-11 07:04:39 +0800

    Fixed css error

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,8 +5,7 @@ import { Account, Role } from './_models';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
     Role = Role;
